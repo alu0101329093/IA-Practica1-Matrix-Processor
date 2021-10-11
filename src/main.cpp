e03c4389179c952431a3c04b38ecf6af715f5889
Jaime <alu0101329093@ull.edu.es>
2021-10-11 18:40:15 +0100

    Init

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+#include "sio_client.h"
+
+std::vector<std::vector<int>> GenerateRandomMatrix() {
+  std::vector<std::vector<int>> matrix(100, std::vector<int>(100));
+  srand(static_cast<unsigned int>(time(nullptr)));
+  for (auto& row : matrix) {
+    for (auto& value : row) {
+      value = rand() % 6;
+    }
+  }
+  return matrix;
+}
+
+int main(int argc, char* argv[]) {
+  sio::client client;
+  // client.set_logs_quiet();
+  client.connect("ws://localhost:3000");
+  bool exit{};
+  client.socket()->on("stop", [&](sio::event& ev) { exit = true; });
+  while (!exit) {
+    std::vector<std::vector<int>> matrix{GenerateRandomMatrix()};
+    nlohmann::json json{matrix};
+    std::string string{json.dump()};
+    client.socket()->emit("receiveMatrix", string);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+
+  system("pause");
+  return EXIT_SUCCESS;
+}
