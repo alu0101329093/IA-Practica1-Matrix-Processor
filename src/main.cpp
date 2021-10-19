34505c46bba294d3deb86b2ca7764eac328a9995
Jaime <alu0101329093@ull.edu.es>
2021-10-19 15:59:49 +0100

    Change main

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,15 @@
 #include "sio_client.h"
 
 std::vector<std::vector<int>> GenerateRandomMatrix() {
-  std::vector<std::vector<int>> matrix(100, std::vector<int>(100));
+  std::vector<std::vector<int>> matrix(50, std::vector<int>(50));
   srand(static_cast<unsigned int>(time(nullptr)));
   for (auto& row : matrix) {
     for (auto& value : row) {
-      value = rand() % 6;
+      value = rand() % 2;
     }
   }
+  matrix[3][4] = 2;
+  matrix[13][8] = 3;
   return matrix;
 }
 
@@ -31,8 +33,8 @@ int main(int argc, char* argv[]) {
     std::string string{json.dump()};
     client.socket()->emit("receiveMatrix", string);
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
-
+  }std
+  std::cout << "Exit" << std::endl;
   // system("pause");
   return EXIT_SUCCESS;
 }
