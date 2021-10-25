28ff49e42246a896a15d3898237a830d0496fb15
Jaime <alu0101329093@ull.edu.es>
2021-10-25 10:41:43 +0100

    Added receive matrix and path

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <chrono>
 #include <ctime>
 #include <iostream>
@@ -8,32 +9,20 @@
 #include "nlohmann/json.hpp"
 #include "sio_client.h"
 
-std::vector<std::vector<int>> GenerateRandomMatrix() {
-  std::vector<std::vector<int>> matrix(50, std::vector<int>(50));
-  srand(static_cast<unsigned int>(time(nullptr)));
-  for (auto& row : matrix) {
-    for (auto& value : row) {
-      value = rand() % 2;
-    }
-  }
-  matrix[3][4] = 2;
-  matrix[13][8] = 3;
-  return matrix;
-}
-
 int main(int argc, char* argv[]) {
   sio::client client;
   // client.set_logs_quiet();
   client.connect("ws://localhost:3000");
   bool exit{};
   client.socket()->on("stop", [&](sio::event& ev) { exit = true; });
-  while (!exit) {
-    std::vector<std::vector<int>> matrix{GenerateRandomMatrix()};
-    nlohmann::json json{matrix};
-    std::string string{json.dump()};
-    client.socket()->emit("receiveMatrix", string);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
+  client.socket()->on("sendMatrix", [&](sio::event& ev) {
+    nlohmann::json json{nlohmann::json::parse(ev.get_message()->get_string())};
+    std::vector<std::vector<int>> matrix =
+        json[0].get<std::vector<std::vector<int>>>();
+    nlohmann::json path_json{{{0, 0}, {1, 1}}};
+    client.socket()->emit("receivePath", path_json.dump());
+  });
+  while (!exit) continue;
   std::cout << "Exit" << std::endl;
   return EXIT_SUCCESS;
 }
